e7c38c37e9cdcfc341e54615a2c76dbc0696b7f5
ashi-kakkar20 <38372085+ashi-kakkar20@users.noreply.github.com>
2018-08-03 22:08:10 +0530

    Add files via upload

--- src/App.js
+++ src/App.js
@@ -3,7 +3,8 @@ import './App.css';
 import Removesboxes from './Removal_Boxes/Removal_boxes';
 
 class App extends Component {
-  state={ Text : ""}
+  state={ Text : "",
+        charArray:[]}
   handleTextChange = (event) => {
     this.setState({
       Text:event.target.value
@@ -11,14 +12,37 @@ class App extends Component {
     });
  }
 
-ChangeHandler = (event) => {
-  var currentArray = this.state.Text;
-  console.log(currentArray);
-   this.setState({
-    Text:currentArray
-  });
+// ChangeHandler = (event) => {
+//   var currentArray = this.state.Text;
+//   console.log(currentArray);
+//    this.setState({
+//     Text:currentArray
+//   });
   
+//   }
+
+
+  ChangeHandler = (event) => {
+    var currentArray = this.state.Text.split('');
+    console.log(currentArray);
+     this.setState({
+      charArray:currentArray
+    })
+  };
+
+  RemoveElement = (event) => {
+    var currentArray=this.state.Text.split('');
+    
+    var index = currentArray.indexOf('L');
+    if (index > -1) {
+    currentArray.splice(index, 1);
+    }
+    this.setState({
+      charArray:currentArray
+    })
   }
+
+
   
   render() {
     
@@ -27,7 +51,14 @@ ChangeHandler = (event) => {
       <Removesboxes handleTextChange={this.handleTextChange}  />
       <button className="buttonSubmit" onClick={this.ChangeHandler} > Generate  </button>
       <div>
-      {this.state.Text}
+      
+      {
+        this.state.charArray.map(el => {
+         return(
+          <p className="el" key={index} onClick={this.RemoveElement.bind(this, index)}>   {el}  <br /></p>
+         );
+        })
+       }
       </div>
       </div>
     );
